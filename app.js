7df56a15208f4b33dfdbfa3c2441fc26e93936c4
c9u11 <tjrdud6412@naver.com>
2024-06-08 21:33:35 +0900

    ✨ login

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/mysql.js");
 const bodyParser = require("body-parser");
+const jwt = require('./utils/jwt.js');
 
 const app = express();
 const conn = db.init();
@@ -27,6 +28,24 @@ router.post('/signup', (req, res) => {
   conn.query("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [body.username, body.email, body.password], (err, rows) => {
     if (err) throw err;
     res.send(rows);
+  });
+});
+
+// 로그인
+router.post('/login', (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  conn.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, password], (err, rows) => {
+    if (err) throw err;
+    if (rows.length > 0) {
+      const user = rows[0];
+      const token = jwt.sign({ id: user.id, email: user.email });
+      res.send({ token });
+    } else {
+      res.send({ message: "Invalid email or password" });
+    }
   });
 });
 
@@ -57,7 +76,6 @@ router.post('/categories/selected', (req, res) => {
     res.send(rows);
   });
 });
-
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
