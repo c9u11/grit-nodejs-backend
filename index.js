af4968ca44f377399002c2effdf7901b3475f85d
c9u11 <tjrdud6412@naver.com>
2024-06-08 20:57:39 +0900

    ✨ insert articles

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const db = require("./config/mysql.js");
+const conn = db.init();
 
 const getArticleContent = async (url) => {
   try {
@@ -26,11 +28,11 @@ const getArticleContent = async (url) => {
 const getTopRankArticle = async () => {
   try {
     const datas = [
-      { category: "politics", articles: [] },
-      { category: "national", articles: [] },
-      { category: "international", articles: [] },
-      { category: "medical", articles: [] },
-      { category: "sports", articles: [] },
+      { id: 1, category: "politics", articles: [] },
+      { id: 2, category: "national", articles: [] },
+      { id: 3, category: "international", articles: [] },
+      { id: 4, category: "medical", articles: [] },
+      { id: 5, category: "sports", articles: [] },
     ];
     for (data of datas) {
       const page = await axios.get(`https://www.chosun.com/${data.category}`);
@@ -61,5 +63,18 @@ const getTopRankArticle = async () => {
 };
 
 getTopRankArticle().then((topRankArticles) => {
-  console.log(JSON.stringify(topRankArticles));
+  topRankArticles.forEach((topRankArticle) => {
+    console.log(topRankArticle.id, topRankArticle.category);
+    topRankArticle.articles.forEach((article, idx) => {
+      if (idx > 2) return;
+      const splitUrl = article.url.split("/");
+      const numberIdx = splitUrl.findIndex((url) => url !== "" && !isNaN(url));
+      const date = `${splitUrl[numberIdx]}-${splitUrl[numberIdx + 1]}-${splitUrl[numberIdx + 2]}`
+      // const date = `${splitUrl[3]}-${splitUrl[4]}-${splitUrl[5]}`;
+      conn.query("INSERT INTO articles (category_id, title, body, url, created_at) VALUES (?, ?, ?, ?, ?)", [topRankArticle.id, article.title, article.url, article.body, date], (err, rows) => {
+        if (err) throw err;
+      });
+    });
+  });
+  console.log("end");
 });
